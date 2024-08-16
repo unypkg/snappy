fc9c06c54abc228c458a941ad8ff0e8a5e6f0b08
michacassola <mich@casso.la>
2024-08-16 09:12:56 +0200

    Update unypkg-snappy-build.sh

--- unypkg-snappy-build.sh
+++ unypkg-snappy-build.sh
@@ -11,7 +11,7 @@ set -vx
 wget -qO- uny.nu/pkg | bash -s buildsys
 
 ### Installing build dependencies
-#unyp install python expat openssl
+unyp install cmake
 
 #pip3_bin=(/uny/pkg/python/*/bin/pip3)
 #"${pip3_bin[0]}" install --upgrade pip
@@ -35,13 +35,13 @@ mkdir -pv /uny/sources
 cd /uny/sources || exit
 
 pkgname="snappy"
-pkggit="https://github.com/snappy/snappy.git refs/tags/*"
+pkggit="https://github.com/google/snappy.git refs/tags/*"
 gitdepth="--depth=1"
 
 ### Get version info from git remote
 # shellcheck disable=SC2086
-latest_head="$(git ls-remote --refs --tags --sort="v:refname" $pkggit | grep -E "v[0-9.]+$" | tail --lines=1)"
-latest_ver="$(echo "$latest_head" | grep -o "v[0-9.].*" | sed "s|v||")"
+latest_head="$(git ls-remote --refs --tags --sort="v:refname" $pkggit | grep -E "/[0-9.]+$" | tail --lines=1)"
+latest_ver="$(echo "$latest_head" | grep -o "/[0-9.].*" | sed "s|/||")"
 latest_commit_id="$(echo "$latest_head" | cut --fields=1)"
 
 version_details
@@ -51,9 +51,9 @@ echo "newer" >release-"$pkgname"
 
 git_clone_source_repo
 
-#cd "$pkgname" || exit
-#./autogen.sh
-#cd /uny/sources || exit
+cd "$pkgname" || exit
+git submodule update --init
+cd /uny/sources || exit
 
 archiving_source
 
@@ -77,11 +77,12 @@ get_include_paths
 
 unset LD_RUN_PATH
 
-./configure \
-    --prefix=/uny/pkg/"$pkgname"/"$pkgver"
+mkdir build
+cd build || exit
+
+cmake -DCMAKE_INSTALL_PREFIX=/uny/pkg/"$pkgname"/"$pkgver" ..
 
 make -j"$(nproc)"
-make -j"$(nproc)" check 
 make -j"$(nproc)" install
 
 ####################################################
